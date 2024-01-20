ca021e440f54c5fbeb6a1034062f0a41187a18a6
= <=>
2024-01-20 23:43:02 +0530

    refactored code

--- app/services/open_weather_map.rb
+++ app/services/open_weather_map.rb
@@ -9,8 +9,7 @@ class OpenWeatherMap
   HISTORICAL_DATA_URI = 'http://api.openweathermap.org/data/2.5/air_pollution/history'
 
   def self.fetch_geocoding_data(city_name)
-    response = get("#{GEOCODING_DATA_URI}?q=#{CGI.escape(city_name)}&limit=1&appid=#{ENV['API_KEY']}")
-    api_log(GEOCODING_DATA_URI, 'geo', "{q: #{city_name}}", 'get', response.body)
+    response = call_api_and_log(GEOCODING_DATA_URI, 'geo', {q: city_name, limit: 1}, 'GET')
 
     return nil unless response.success?
     parsed_response = JSON.parse(response.body)
@@ -24,31 +23,29 @@ class OpenWeatherMap
   end
 
   def self.fetch_air_quality(location)
-    city_name, lon, lat = location.name, location.longitude, location.latitude
-    response = get("#{AIR_POLLUTION_URI}?lat=#{lat}&lon=#{lon}&appid=#{ENV['API_KEY']}")
-    api_log(AIR_POLLUTION_URI, 'curr_pollution', "{lat: #{lat}, lon: #{lon}}", 'get', response.body)
+    lon, lat = location.longitude, location.latitude
+    response = call_api_and_log(AIR_POLLUTION_URI, 'curr_pollution', {lat: lat, lon: lon}, 'GET')
 
     return nil unless response.success?
-
     parsed_response = JSON.parse(response.body)
   end
 
   def self.fetch_historical_data(location, period_in_days = 365)
-    latititude, longitude = location.latitude, location.longitude
+    lat, lon = location.latitude, location.longitude
     start_time, end_time = (Time.now-period_in_days.days).utc.to_i, Time.now.utc.to_i 
-
-    response = get("#{HISTORICAL_DATA_URI}?lat=#{latititude}&lon=#{longitude}&start=#{start_time}&end=#{end_time}&appid=#{ENV['API_KEY']}")
-    api_log(HISTORICAL_DATA_URI, 'historic', "{lat: #{latititude}, lon: #{longitude}, start: #{start_time}, end: #{end_time}}", 'get', response)
+    response = call_api_and_log(HISTORICAL_DATA_URI, 'historic_pollution', {lat: lat, lon: lon, start: start_time, end: end_time}, 'GET')
 
     return nil unless response.success?
-
     parsed_response = JSON.parse(response.body)
   end
 
   private
 
-  def self.api_log(url, name, params, type, response)
-    ApiCallLog.create(api_name: name, api_end_point: url, params: params, request_type: type, callback: response)
+  def self.call_api_and_log(url, name, params, type)
+    params[:appid] = ENV['API_KEY']
+    response = get(url, query: params)
+    ApiCallLog.create(api_name: name, api_end_point: url, params: params.to_s, request_type: type, callback: response)
+    response
   end
 
 end
